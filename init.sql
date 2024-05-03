a2fe2fd4898a5c568b9ed4634b7c2b9dfdcd59c7
Joshua Kravitz <2271524@johnabbottcollege.net>
2024-05-03 14:00:23 +0000

    Implemented database.

--- init.sql
+++ init.sql
@@ -4,6 +4,64 @@ CREATE DATABASE "MyDB";
 \c MyDB;
 
 DROP TABLE IF EXISTS table_name;
-CREATE TABLE table_name (
-    id SERIAL PRIMARY KEY,
-);
+CREATE TABLE user {
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(100) NOT NULL UNIQUE,
+        password VARCHAR(200),
+        email VARCHAR(100)
+    }
+
+CREATE TABLE pokemon_species {
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        type VARCHAR(50),
+        userImageURL VARCHAR(255)
+    }
+
+CREATE TABLE box {
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        userId INTEGER REFERENCES users(id)       
+    }
+
+CREATE TABLE box_species {
+        id SERIAL PRIMARY KEY,
+        pokemonId INTEGER REFERENCES pokemon_species(id),
+        userId INTEGER REFERENCES users(id),
+        boxId INTEGER REFERENCES box(id),
+        level INTEGER,
+        nature VARCHAR(50),
+        ability VARCHAR(50)
+    }
+
+CREATE TABLE team {
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        userId INTEGER REFERENCES users(id)
+    }
+
+CREATE TABLE team_positions {
+        teamId INT,
+        boxId INT,
+        position INT,
+        PRIMARY KEY (teamId, boxId),
+        FOREIGN KEY (teamId) REFERENCES team(id),
+        FOREIGN KEY (boxId) REFERENCES box(id)
+    }
+
+CREATE TABLE moves {
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        accuracy INT,
+        effect_chance INT,
+        pp INT,   
+        power INT
+    }
+
+CREATE TABLE pokemon_moves {
+        box_speciesId INT,
+        moveId INT,
+        PRIMARY KEY (box_speciesId, moveId), -- Composite primary key
+        FOREIGN KEY (box_speciesId) REFERENCES box(id),
+        FOREIGN KEY (moveId) REFERENCES moves(id)
+    }
